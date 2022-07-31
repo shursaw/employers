724c4feb31eafba418647d6f8a2497fe26c20390
Alexander <shursaw@gmail.com>
2022-07-31 23:40:17 +0300

    010

---
+++ src/components/app/app.js
@@ -0,0 +1,32 @@
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
+
+import './app.css';
+
+function App() {
+
+  const data = [
+    {name: 'Alex K.', salary: 800, increase: false},
+    {name: 'John D.', salary: 3000, increase: true},
+    {name: 'Juty M.', salary: 500, increase: false},
+  ];
+
+  return (
+    <div className="app">
+        <AppInfo />
+
+        <div className="search-panel">
+            <SearchPanel/>
+            <AppFilter/>
+        </div>
+        
+        <EmployeesList data={data}/>
+        <EmployeesAddForm/>
+    </div>
+  );
+}
+
+export default App;
